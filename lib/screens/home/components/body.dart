a1eeba71ed33ca36f3da179c59b9316128ec54ef
Rimma <rimma02011998@gmail.com>
2020-11-27 16:17:03 +0200

    fixed ListViev with widget Card

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -11,31 +11,30 @@ class _MainBodyState extends State<MainBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.separated(
+      color: Colors.white,
+      child: ListView.builder(
         itemCount: TestList().list.length,
-        separatorBuilder: (_ , __) => Divider(
-          height: 5.0,
-          thickness: 3.0,
-          color: Colors.lightBlue,
-        ),
         itemBuilder: (_, index) {
-          return ListTile(
-            title: Text(DateTime.now().toString()),
-            subtitle: Text(
-                TestList().list[index],
-              style: TextStyle(fontSize: 31.0),
+          return Card(
+            elevation: 10,
+            shadowColor: Colors.lightBlue,
+            margin: EdgeInsets.symmetric(vertical: 7),
+            child: ListTile(
+              title: Text(DateTime.now().toString()),
+              subtitle: Text(
+                  TestList().list[index],
+                style: TextStyle(fontSize: 31.0),
+              ),
+              onTap: () {print("${index+1} ${TestList().list[index]}");},
+              trailing: Icon(
+                Icons.ac_unit_outlined,
+              ),
+              isThreeLine: false,
             ),
-            onTap: () {print("${index+1} ${TestList().list[index]}");},
-            trailing: Icon(
-              Icons.ac_unit_outlined,
-            ),
-            isThreeLine: false,
           );
         },
       ),
     );
   }
 }
-// (_, index) => Text(
-// TestList().list[index],
-// style: TextStyle(fontSize: 50),
+
